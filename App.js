b04a13bea7d3e6fbfa9404ca74eb11d66e6aa9cc
Badar Butt <211370113@gift.edu.pk>
2024-08-17 11:47:35 +0500

    done

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import {Text, View} from 'react-native';
 import React from 'react';
 import AppNavigation from './navigation/AppNavigation';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import {Provider} from 'react-redux';
+import {store} from './redux/store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <AppNavigation/>
+      <AppNavigation />
     </Provider>
   );
 };
